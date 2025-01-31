7689dd11873d3a8b6fde1adea61e5ed53aa35b80
Deirror <alexmailbg@gmail.com>
2025-01-31 17:24:23 +0200

    add source code so far

---
+++ services/orders/types/types.go
@@ -0,0 +1,11 @@
+package types
+
+import (
+	"context"
+
+	"github.com/Deirror/gRPC-Kitchen/services/common/genproto/orders"
+)
+
+type OrderService interface {
+	CreateOrder(context.Context, *orders.Order) error
+}
